95cee1bf8d001912a500073048c57ab572faed54
Eugene Kazakov <ekazakov@parallels.com>
2014-07-21 10:29:19 +0700

    Add copyrights.

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,4 +1,4 @@
 <?php
-
+// Copyright 1999-2014. Parallels IP Holdings GmbH. All Rights Reserved.
 $application = new pm_Application();
 $application->run();
--- plib/resources/locales/en-US.php
+++ plib/resources/locales/en-US.php
@@ -1,5 +1,5 @@
 <?php
-
+// Copyright 1999-2014. Parallels IP Holdings GmbH. All Rights Reserved.
 $messages = [
     'settingsPageTitle' => 'LDAP Auth Settings',
     'settingsSaved' => 'The settings were saved.',
