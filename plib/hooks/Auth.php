40742ade74cb957901078d046c7d870d31a5a1c0
Alexei Yuzhakov <ayuzhakov@parallels.com>
2015-03-22 21:04:53 +0600

    Update copyright year.

--- plib/hooks/Auth.php
+++ plib/hooks/Auth.php
@@ -1,5 +1,5 @@
 <?php
-// Copyright 1999-2014. Parallels IP Holdings GmbH. All Rights Reserved.
+// Copyright 1999-2015. Parallels IP Holdings GmbH. All Rights Reserved.
 class Modules_LdapAuth_Auth extends pm_Hook_Auth
 {
 
