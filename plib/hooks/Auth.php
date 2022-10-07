a75b63cc629a048dff2aeebeb7c1ff761779a67a
Dmitriy Medvedev <dmedvedev@plesk.com>
2022-10-07 16:52:28 +0300

    EXTPLESK-3999 Fixed Windows AD authentication via SSL:TLS.

--- plib/hooks/Auth.php
+++ plib/hooks/Auth.php
@@ -16,6 +16,8 @@ class Modules_LdapAuth_Auth extends pm_Hook_Auth
         curl_setopt($ch, CURLOPT_URL, $protocol . pm_Settings::get('host') . ":" . $port);
         curl_setopt($ch, CURLOPT_HEADER, false);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        // Force closing TLS channel after login
+        curl_setopt($ch, CURLOPT_CONNECT_ONLY, true);
         curl_setopt($ch, CURLOPT_USERPWD, pm_Settings::get('loginPrefix') . $login . pm_Settings::get('loginSuffix') . ":" . $password);
         \pm_Log::debug('Sending a request to the LDAP server');
         $result = curl_exec($ch);
